6eff2ce9575489f68bb8e3c4a1acc2bb68960b95
gamasenninn <gamasenninn@yahoo.co.jp>
2022-08-07 14:12:15 +0900

    spec pattern 設定

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -3,6 +3,7 @@ module.exports = {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    supportFile: false
+    supportFile: false,
+    specPattern: 'e2e/**/*.cy.{js,jsx,ts,tsx}'
   },
 };
